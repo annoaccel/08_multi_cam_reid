8d309134dccfacd98fbfebbb3c5a39869c864050
annoaccel <106055405+annoaccel@users.noreply.github.com>
2022-11-09 11:19:35 +0530

    Update run1.py

--- run1.py
+++ run1.py
@@ -11,6 +11,9 @@ oo = "--version v3"
 
 pp = "python" + " " + "demo.py" + " " + kk + " " + jj + " " + hh + " " + oo
 
+subprocess.call('python convert_y3.py model_data/weights/yolov3.weights model_data/models/yolov3.h5', shell=True)
+
+subprocess.call('python convert_y4.py model_data/weights/yolov4.weights model_data/models/yolov4.h5', shell=True)
 
 subprocess.call(
     pp,
